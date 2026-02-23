fd208bd956f9dfcd408cc0a96da735e7f64a3f27
JSode64 <jsode6405@gmail.com>
2026-02-22 20:57:07 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const url = "https://api.nysmesonet.org/data/index.php/geojson/etec/latest";
+const response = await fetch(url);
+const json = await response.json();
+const properties = json.features[0].properties;
+
+const [date, time] = properties.PROD_TIME.split("T");
+
+let output = "";
+output += `Date: ${date}\n`;
+output += `Time: ${time}\n`;
+output += `Station Pressure: ${properties.PRES}mb\n`;
+output += `Dry Buld Temperature: ${properties.TAIR}°C\n`;
+output += `Relative Humidity: ${properties.RELH}%\n`;
+output += `Wind Direction: ${properties.WDIR}°\n`;
+output += `Wind Speed: ${properties.WSPD}m/s\n`;
+
+const outputText = document.getElementById("output-text");
+outputText.textContent = output;
